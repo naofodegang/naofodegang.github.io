78071788060542c32ecfa644122222817aa55523
naofodegang <pedropassarello@gmail.com>
2024-09-11 00:15:47 -0300

    First Upload
    First files upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let themeToggle = document.getElementById("themeToggle");
+let body = document.querySelector("body");
+let h1 = document.querySelector("h1");
+let containers = document.querySelectorAll(".container, .container2");
+let navbar = document.querySelector(".navbar");
+let tracks = document.querySelectorAll(".tracks, .tracks2");
+let img = document.getElementById("meninossmt")
+
+themeToggle.addEventListener("click", () => {
+    body.classList.toggle("dark");
+    h1.classList.toggle("dark");
+    
+    containers.forEach(container => container.classList.toggle("dark"));
+    navbar.classList.toggle("dark");
+    tracks.forEach(track => track.classList.toggle("dark"));
+
+    // Troca o ícone do botão
+    if (body.classList.contains("dark")) {
+        themeToggle.innerHTML = "🌙"; 
+        img.src = "/capa do naofuck.png"; 
+    } else {
+        themeToggle.innerHTML = "☀️"; 
+        img.src = "/meninossmt.gif"; 
+    }
+});
